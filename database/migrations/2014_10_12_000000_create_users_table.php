53a449be589096ec8bd0acdbc31249729a0ce5b4
KoPinky <mrlongyst@gmail.com>
2021-02-18 11:16:11 +0700

    make migration and init model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('surname');
+            $table->string('firstname');
+            $table->string('secondname');
+            $table->string('login');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('role_id')->references('id')->on('users');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_02_18_033009_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_contract');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->boolean('status');
+            $table->bigInteger('object_id')->references('id')->on('objects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
